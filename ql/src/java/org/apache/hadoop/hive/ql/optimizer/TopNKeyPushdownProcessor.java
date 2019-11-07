c42ce37b53696c62164291f9aa13832fe7bb252d
kkasa <kkasa@cloudera.com>
2019-11-07 09:59:33 +0100

    HIVE-20150: TopNKey pushdown - push through other TNK

--- ql/src/java/org/apache/hadoop/hive/ql/optimizer/TopNKeyPushdownProcessor.java
+++ ql/src/java/org/apache/hadoop/hive/ql/optimizer/TopNKeyPushdownProcessor.java
@@ -42,9 +42,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Stack;
-import java.util.stream.Stream;
 
 import static org.apache.hadoop.hive.ql.optimizer.TopNKeyProcessor.copyDown;
 
