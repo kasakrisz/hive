0943ac8426fb96c2c8f55ec535f5f8d6e762a146
kkasa <kkasa@cloudera.com>
2019-11-26 11:08:31 +0100

    HIVE-20150: TopNKey pushdown - refac CommonPrefix

--- ql/src/java/org/apache/hadoop/hive/ql/optimizer/CommonKeyPrefix.java
+++ ql/src/java/org/apache/hadoop/hive/ql/optimizer/CommonKeyPrefix.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.hadoop.hive.ql.exec;
+package org.apache.hadoop.hive.ql.optimizer;
 
 import java.util.ArrayList;
 import java.util.List;
