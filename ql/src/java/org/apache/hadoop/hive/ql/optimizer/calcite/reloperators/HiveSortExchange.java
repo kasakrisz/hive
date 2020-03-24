945123a36ce084e5dbcda2b05d3bdb25d0e902d1
kkasa <kkasa@cloudera.com>
2020-03-24 20:48:47 +0100

    HIVE-22785: Update/delete/merge statements not optimized through CBO - refac: create HiveSortExchange

--- ql/src/java/org/apache/hadoop/hive/ql/optimizer/calcite/reloperators/HiveSortExchange.java
+++ ql/src/java/org/apache/hadoop/hive/ql/optimizer/calcite/reloperators/HiveSortExchange.java
@@ -21,9 +21,11 @@ package org.apache.hadoop.hive.ql.optimizer.calcite.reloperators;
 import org.apache.calcite.plan.RelOptCluster;
 import org.apache.calcite.plan.RelTraitSet;
 import org.apache.calcite.rel.RelCollation;
+import org.apache.calcite.rel.RelCollationImpl;
 import org.apache.calcite.rel.RelCollationTraitDef;
 import org.apache.calcite.rel.RelDistribution;
 import org.apache.calcite.rel.RelDistributionTraitDef;
+import org.apache.calcite.rel.RelFieldCollation;
 import org.apache.calcite.rel.RelNode;
 import org.apache.calcite.rel.core.SortExchange;
 import org.apache.calcite.rex.RexNode;
@@ -52,7 +54,7 @@ public final class HiveSortExchange extends SortExchange implements HiveRelNode 
    * @param input     Input relational expression
    * @param distribution Distribution specification
    * @param collation Collation specification
-   * @param keys Join Keys specification
+   * @param keys Keys specification
    */
   public static HiveSortExchange create(RelNode input,
       RelDistribution distribution, RelCollation collation, ImmutableList<RexNode> keys) {
@@ -61,6 +63,23 @@ public final class HiveSortExchange extends SortExchange implements HiveRelNode 
     collation = RelCollationTraitDef.INSTANCE.canonize(collation);
     RelTraitSet traitSet = TraitsUtil.getDefaultTraitSet(cluster).replace(collation);
     return new HiveSortExchange(cluster, traitSet, input, distribution, collation, keys);
+  }
+
+  public static HiveSortExchange create(RelNode input,
+      RelDistribution distribution, RelCollation collation) {
+    RelOptCluster cluster = input.getCluster();
+    distribution = RelDistributionTraitDef.INSTANCE.canonize(distribution);
+    collation = RelCollationTraitDef.INSTANCE.canonize(collation);
+    RelTraitSet traitSet = TraitsUtil.getDefaultTraitSet(cluster).replace(collation);
+    RelCollation canonizedCollation = traitSet.canonize(RelCollationImpl.of(collation.getFieldCollations()));
+
+    ImmutableList.Builder<RexNode> builder = ImmutableList.builder();
+    for (RelFieldCollation relFieldCollation : canonizedCollation.getFieldCollations()) {
+      int index = relFieldCollation.getFieldIndex();
+      builder.add(cluster.getRexBuilder().makeInputRef(input, index));
+    }
+
+    return new HiveSortExchange(cluster, traitSet, input, distribution, collation, builder.build());
   }
 
   @Override
