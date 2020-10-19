ab3d48fd50cbc4727580dd118ed2ae20bc5c1c7e
kasakrisz <kasakrisz2@gmail.com>
2020-10-19 11:54:35 +0200

    Overwrite TokenRewriteStream in Context after reparsing with masking since we have a new query.

--- ql/src/java/org/apache/hadoop/hive/ql/Context.java
+++ ql/src/java/org/apache/hadoop/hive/ql/Context.java
@@ -979,7 +979,6 @@ public class Context {
    *          the stream being used
    */
   public void setTokenRewriteStream(TokenRewriteStream tokenRewriteStream) {
-    assert (this.tokenRewriteStream == null || this.getExplainAnalyze() == AnalyzeState.RUNNING);
     this.tokenRewriteStream = tokenRewriteStream;
   }
 
