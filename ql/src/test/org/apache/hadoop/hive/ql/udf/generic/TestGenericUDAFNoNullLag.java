80e3d10171d7340b7482ea72249bd67ff3d3bcbd
kasakrisz <kasakrisz2@gmail.com>
2021-07-13 15:40:57 +0200

    HIVE-24869: Implement Respect/Ignore Nulls in lag/lead - lag

---
+++ ql/src/test/org/apache/hadoop/hive/ql/udf/generic/TestGenericUDAFNoNullLag.java
@@ -0,0 +1,105 @@
+package org.apache.hadoop.hive.ql.udf.generic;
+
+import org.apache.hadoop.hive.ql.metadata.HiveException;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
+import org.apache.hadoop.hive.serde2.typeinfo.TypeInfoFactory;
+import org.apache.hadoop.hive.serde2.typeinfo.TypeInfoUtils;
+import org.apache.hadoop.io.IntWritable;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+class TestGenericUDAFNoNullLag {
+
+  GenericUDAFLag.GenericUDAFLagEvaluatorStreaming evaluator;
+
+  @BeforeEach
+  void setUp() throws HiveException {
+    GenericUDAFLag.GenericUDAFNoNullLagEvaluator baseEvaluator = new GenericUDAFLag.GenericUDAFNoNullLagEvaluator();
+    ObjectInspector[] inputIO = new ObjectInspector[] {
+            TypeInfoUtils.getStandardWritableObjectInspectorFromTypeInfo(TypeInfoFactory.intTypeInfo)
+    };
+    baseEvaluator.init(GenericUDAFEvaluator.Mode.COMPLETE, inputIO);
+
+    evaluator = new GenericUDAFLag.GenericUDAFLagEvaluatorStreaming(baseEvaluator);
+  }
+
+  private Object[] parameters(Object value) {
+    Object[] parameters = new Object[1];
+    parameters[0] = value == null ? null : new IntWritable((Integer) value);
+    return parameters;
+  }
+
+  private GenericUDAFLag.NoNullLagBuffer leadBuffer() {
+    GenericUDAFLag.NoNullLagBuffer buffer = new GenericUDAFLag.NoNullLagBuffer();
+    buffer.initialize(2);
+    return buffer;
+  }
+
+  @Test
+  void testIgnoreNulls() throws HiveException {
+    GenericUDAFLag.NoNullLagBuffer buffer = leadBuffer();
+
+    evaluator.iterate(buffer, parameters(8));
+    assertThat(evaluator.getNextResult(buffer), is(ISupportStreamingModeForWindowing.NULL_RESULT));
+
+    evaluator.iterate(buffer, parameters(null));
+    assertThat(evaluator.getNextResult(buffer), is(ISupportStreamingModeForWindowing.NULL_RESULT));
+
+    evaluator.iterate(buffer, parameters(null));
+    assertThat(evaluator.getNextResult(buffer), is(ISupportStreamingModeForWindowing.NULL_RESULT));
+
+    evaluator.iterate(buffer, parameters(5));
+    assertThat(evaluator.getNextResult(buffer), is(ISupportStreamingModeForWindowing.NULL_RESULT));
+
+    evaluator.iterate(buffer, parameters(4));
+    assertThat(evaluator.getNextResult(buffer), is(new IntWritable(8)));
+
+    evaluator.iterate(buffer, parameters(null));
+    assertThat(evaluator.getNextResult(buffer), is(new IntWritable(5)));
+
+    evaluator.iterate(buffer, parameters(2));
+    assertThat(evaluator.getNextResult(buffer), is(new IntWritable(5)));
+
+    evaluator.iterate(buffer, parameters(1));
+    assertThat(evaluator.getNextResult(buffer), is(new IntWritable(4)));
+
+    evaluator.terminate(buffer);
+  }
+
+  @Test
+  void testIgnoreNullsWhenAllInputValuesAreNull() throws HiveException {
+    GenericUDAFLag.NoNullLagBuffer buffer = leadBuffer();
+
+    evaluator.iterate(buffer, parameters(null));
+    assertThat(evaluator.getNextResult(buffer), is(nullValue()));
+
+    evaluator.iterate(buffer, parameters(null));
+    assertThat(evaluator.getNextResult(buffer), is(nullValue()));
+
+    evaluator.terminate(buffer);
+
+    assertThat(evaluator.getNextResult(buffer), is(ISupportStreamingModeForWindowing.NULL_RESULT));
+    assertThat(evaluator.getNextResult(buffer), is(ISupportStreamingModeForWindowing.NULL_RESULT));
+  }
+}
