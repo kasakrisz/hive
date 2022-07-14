170a26a1bf38b91df056e9ed48746aea1ac9e050
kkasa <kkasa@cloudera.com>
2022-07-14 14:58:05 +0200

    extract MergeSemanticAnalyzer

--- ql/src/java/org/apache/hadoop/hive/ql/parse/SemanticAnalyzerFactory.java
+++ ql/src/java/org/apache/hadoop/hive/ql/parse/SemanticAnalyzerFactory.java
@@ -101,6 +101,10 @@ public final class SemanticAnalyzerFactory {
         return new UpdateDeleteSemanticAnalyzer(queryState);
 
       case HiveParser.TOK_MERGE:
+        if (HiveConf.getBoolVar(queryState.getConf(), HiveConf.ConfVars.SPLIT_UPDATE) ||
+            HiveConf.getBoolVar(queryState.getConf(), HiveConf.ConfVars.MERGE_SPLIT_UPDATE)) {
+          return new SplitMergeSemanticAnalyzer(queryState);
+        }
         return new MergeSemanticAnalyzer(queryState);
 
       case HiveParser.TOK_ALTER_SCHEDULED_QUERY:
