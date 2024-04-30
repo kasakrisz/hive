eba7467a717cdc0cb5a6637b97a4fbeefb4296a3
kasakrisz <kasakrisz2@gmail.com>
2024-04-30 14:30:05 +0200

    Add UT of Context.getMRTmpPath

--- ql/src/test/org/apache/hadoop/hive/ql/exec/TestContext.java
+++ ql/src/test/org/apache/hadoop/hive/ql/exec/TestContext.java
@@ -19,15 +19,24 @@
 package org.apache.hadoop.hive.ql.exec;
 
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hive.common.FileUtils;
 import org.apache.hadoop.hive.conf.HiveConf;
 import org.apache.hadoop.hive.ql.Context;
+import org.apache.hadoop.hive.ql.parse.ExplainConfiguration;
 import org.apache.hadoop.hive.ql.session.SessionState;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.*;
 
 public class TestContext {
@@ -69,5 +78,203 @@ public class TestContext {
         assertEquals(tmpPathRelTo, spyContext.getTempDirForInterimJobPath(new Path("s3a://bucket/dir")));
         assertEquals(mrTmpPath, spyContext.getTempDirForInterimJobPath(new Path("file:///user")));
         conf.setBoolean(HiveConf.ConfVars.HIVE_BLOBSTORE_OPTIMIZATIONS_ENABLED.varname, true);
+    }
+
+    @Test
+    public void testLocalGetMRTmpPathCreatesTheFolder() {
+        Context ctx = new Context(conf);
+        Path path = ctx.getMRTmpPath();
+        assertTrue(Files.exists(Paths.get(path.getParent().toUri())));
+    }
+
+    @Test
+    public void testLocalGetMRTmpPathDoesNotCreatesTheFolderWhenMVIsLoading() {
+        Context ctx = new Context(conf);
+        ctx.setIsLoadingMaterializedView(true);
+        Path path = ctx.getMRTmpPath();
+        assertFalse(Files.exists(Paths.get(path.getParent().toUri())));
+    }
+
+    @Test
+    public void testLocalGetMRTmpPathDoesNotCreatesTheFolderWhenExplain() {
+        Context ctx = new Context(conf);
+        ctx.setExplainConfig(new ExplainConfiguration());
+        Path path = ctx.getMRTmpPath();
+        assertFalse(Files.exists(Paths.get(path.getParent().toUri())));
+    }
+
+    @Test
+    public void testLocalGetMRTmpPathDoesNotCreatesTheFolderWhenFalsePassed() {
+        Context ctx = new Context(conf);
+        Path path = ctx.getMRTmpPath(false);
+        assertFalse(Files.exists(Paths.get(path.getParent().toUri())));
+    }
+
+    @Test
+    public void testLocalGetMRTmpPathCreatesTheFolderWhenTruePassed() {
+        Context ctx = new Context(conf);
+        Path path = ctx.getMRTmpPath(true);
+        assertTrue(Files.exists(Paths.get(path.getParent().toUri())));
+    }
+
+    @Test
+    public void testLocalGetMRTmpPathCreatesTheFolderWhenTruePassedEvenWhenMVLoading() {
+        Context ctx = new Context(conf);
+        ctx.setIsLoadingMaterializedView(true);
+        Path path = ctx.getMRTmpPath(true);
+        assertTrue(Files.exists(Paths.get(path.getParent().toUri())));
+    }
+
+    @Test
+    public void testLocalGetMRTmpPathCreatesTheFolderWhenTruePassedEvenWhenExplainPlan() {
+        Context ctx = new Context(conf);
+        ctx.setExplainConfig(new ExplainConfiguration());
+        Path path = ctx.getMRTmpPath(true);
+        assertTrue(Files.exists(Paths.get(path.getParent().toUri())));
+    }
+
+    @Test
+    public void testLocalGetMRTmpPathCreatesTheFolderWhenTablePathPassed() throws URISyntaxException, IOException {
+        String anyTableLocation = "anyTableLocation";
+        FileUtils.deleteDirectory(new File(anyTableLocation));
+
+        Context ctx = new Context(conf);
+        Path path = ctx.getMRTmpPath(new URI(anyTableLocation));
+        assertTrue(Files.exists(Paths.get(path.getParent().toUri())));
+    }
+
+    @Test
+    public void testLocalGetMRTmpPathCreatesTheFolderWhenTablePathPassedAndMVIsLoading() throws URISyntaxException, IOException {
+        Context ctx = new Context(conf);
+        ctx.setIsLoadingMaterializedView(true);
+        Path path = ctx.getMRTmpPath(new URI("anyTableLocation"));
+        assertFalse(Files.exists(Paths.get(path.getParent().toUri())));
+    }
+
+    @Test
+    public void testLocalGetMRTmpPathCreatesTheFolderWhenTablePathPassedAndExplainPlan() throws URISyntaxException, IOException {
+        Context ctx = new Context(conf);
+        ctx.setExplainConfig(new ExplainConfiguration());
+        Path path = ctx.getMRTmpPath(new URI("anyTableLocation"));
+        assertFalse(Files.exists(Paths.get(path.getParent().toUri())));
+    }
+
+    @Test
+    public void testGetMRTmpPathCreatesTheFolder() {
+        Context ctx = new Context(conf);
+        Context spyContext = spy(ctx);
+        doReturn(false).when(spyContext).isLocalOnlyExecutionMode();
+
+        Path path = spyContext.getMRTmpPath();
+
+        assertTrue(Files.exists(Paths.get(path.getParent().toUri())));
+    }
+
+    @Test
+    public void testGetMRTmpPathDoesNotCreatesTheFolderWhenMVIsLoading() {
+        Context ctx = new Context(conf);
+        ctx.setIsLoadingMaterializedView(true);
+        Context spyContext = spy(ctx);
+        doReturn(false).when(spyContext).isLocalOnlyExecutionMode();
+
+        Path path = ctx.getMRTmpPath();
+
+        assertFalse(Files.exists(Paths.get(path.getParent().toUri())));
+    }
+
+    @Test
+    public void testGetMRTmpPathDoesNotCreatesTheFolderWhenExplain() {
+        Context ctx = new Context(conf);
+        ctx.setExplainConfig(new ExplainConfiguration());
+        Context spyContext = spy(ctx);
+        doReturn(false).when(spyContext).isLocalOnlyExecutionMode();
+
+        Path path = ctx.getMRTmpPath();
+
+        assertFalse(Files.exists(Paths.get(path.getParent().toUri())));
+    }
+
+    @Test
+    public void testGetMRTmpPathDoesNotCreatesTheFolderWhenFalsePassed() {
+        Context ctx = new Context(conf);
+        Context spyContext = spy(ctx);
+        doReturn(false).when(spyContext).isLocalOnlyExecutionMode();
+
+        Path path = ctx.getMRTmpPath(false);
+
+        assertFalse(Files.exists(Paths.get(path.getParent().toUri())));
+    }
+
+    @Test
+    public void testGetMRTmpPathCreatesTheFolderWhenTruePassed() {
+        Context ctx = new Context(conf);
+        Context spyContext = spy(ctx);
+        doReturn(false).when(spyContext).isLocalOnlyExecutionMode();
+
+        Path path = ctx.getMRTmpPath(true);
+
+        assertTrue(Files.exists(Paths.get(path.getParent().toUri())));
+    }
+
+    @Test
+    public void testGetMRTmpPathCreatesTheFolderWhenTruePassedEvenWhenMVLoading() {
+        Context ctx = new Context(conf);
+        ctx.setIsLoadingMaterializedView(true);
+        Context spyContext = spy(ctx);
+        doReturn(false).when(spyContext).isLocalOnlyExecutionMode();
+
+        Path path = ctx.getMRTmpPath(true);
+
+        assertTrue(Files.exists(Paths.get(path.getParent().toUri())));
+    }
+
+    @Test
+    public void testGetMRTmpPathCreatesTheFolderWhenTruePassedEvenWhenExplainPlan() {
+        Context ctx = new Context(conf);
+        ctx.setExplainConfig(new ExplainConfiguration());
+        Context spyContext = spy(ctx);
+        doReturn(false).when(spyContext).isLocalOnlyExecutionMode();
+
+        Path path = ctx.getMRTmpPath(true);
+
+        assertTrue(Files.exists(Paths.get(path.getParent().toUri())));
+    }
+
+    @Test
+    public void testGetMRTmpPathCreatesTheFolderWhenTablePathPassed() throws URISyntaxException, IOException {
+        String anyTableLocation = "anyTableLocation";
+        FileUtils.deleteDirectory(new File(anyTableLocation));
+
+        Context ctx = new Context(conf);
+        Context spyContext = spy(ctx);
+        doReturn(false).when(spyContext).isLocalOnlyExecutionMode();
+
+        Path path = ctx.getMRTmpPath(new URI(anyTableLocation));
+
+        assertTrue(Files.exists(Paths.get(path.getParent().toUri())));
+    }
+
+    @Test
+    public void testGetMRTmpPathCreatesTheFolderWhenTablePathPassedAndMVIsLoading() throws URISyntaxException, IOException {
+        Context ctx = new Context(conf);
+        ctx.setIsLoadingMaterializedView(true);
+        Context spyContext = spy(ctx);
+        doReturn(false).when(spyContext).isLocalOnlyExecutionMode();
+
+        Path path = ctx.getMRTmpPath(new URI("anyTableLocation"));
+
+        assertFalse(Files.exists(Paths.get(path.getParent().toUri())));
+    }
+
+    @Test
+    public void testGetMRTmpPathCreatesTheFolderWhenTablePathPassedAndExplainPlan() throws URISyntaxException, IOException {
+        Context ctx = new Context(conf);
+        ctx.setExplainConfig(new ExplainConfiguration());
+        Context spyContext = spy(ctx);
+        doReturn(false).when(spyContext).isLocalOnlyExecutionMode();
+
+        Path path = ctx.getMRTmpPath(new URI("anyTableLocation"));
+
+        assertFalse(Files.exists(Paths.get(path.getParent().toUri())));
     }
 }
