45540060285c15a2d22df2c0ecfe33db0e38dc27
kasakrisz <kasakrisz2@gmail.com>
2025-03-07 11:13:10 +0100

    HiveMaterializationRelMetadataProvider

--- ql/src/java/org/apache/hadoop/hive/ql/optimizer/calcite/rules/views/HiveMaterializationRelMetadataProvider.java
+++ ql/src/java/org/apache/hadoop/hive/ql/optimizer/calcite/rules/views/HiveMaterializationRelMetadataProvider.java
@@ -20,15 +20,20 @@ package org.apache.hadoop.hive.ql.optimizer.calcite.rules.views;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Multimap;
 import java.util.Set;
+
+import org.apache.calcite.plan.RelOptCost;
 import org.apache.calcite.plan.RelOptPredicateList;
 import org.apache.calcite.plan.volcano.RelSubset;
 import org.apache.calcite.rel.RelNode;
+import org.apache.calcite.rel.core.TableScan;
 import org.apache.calcite.rel.metadata.ChainedRelMetadataProvider;
 import org.apache.calcite.rel.metadata.JaninoRelMetadataProvider;
 import org.apache.calcite.rel.metadata.ReflectiveRelMetadataProvider;
 import org.apache.calcite.rel.metadata.RelMdAllPredicates;
 import org.apache.calcite.rel.metadata.RelMdExpressionLineage;
 import org.apache.calcite.rel.metadata.RelMdNodeTypes;
+import org.apache.calcite.rel.metadata.RelMdPercentageOriginalRows;
+import org.apache.calcite.rel.metadata.RelMdRowCount;
 import org.apache.calcite.rel.metadata.RelMdTableReferences;
 import org.apache.calcite.rel.metadata.RelMetadataProvider;
 import org.apache.calcite.rel.metadata.RelMetadataQuery;
@@ -36,7 +41,12 @@ import org.apache.calcite.rex.RexNode;
 import org.apache.calcite.rex.RexTableInputRef.RelTableRef;
 import org.apache.calcite.util.BuiltInMethod;
 import org.apache.calcite.util.Util;
+import org.apache.hadoop.hive.ql.optimizer.calcite.cost.HiveCost;
+import org.apache.hadoop.hive.ql.optimizer.calcite.cost.HiveDefaultCostModel;
+import org.apache.hadoop.hive.ql.optimizer.calcite.cost.HiveRelMdCost;
 import org.apache.hadoop.hive.ql.optimizer.calcite.rules.views.HiveMaterializedViewBoxing.Box;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Metadata provider implementation that is only used for MV rewriting.
@@ -59,10 +69,15 @@ public class HiveMaterializationRelMetadataProvider {
       ReflectiveRelMetadataProvider.reflectiveSource(
           BuiltInMethod.TABLE_REFERENCES.method, new HiveRelMdTableReferences());
 
+  public static final RelMetadataProvider SOURCE_ROW_COUNT = ReflectiveRelMetadataProvider
+      .reflectiveSource(BuiltInMethod.ROW_COUNT.method, new HiveRelMdRowCount());
+
   public static final JaninoRelMetadataProvider DEFAULT =
       JaninoRelMetadataProvider.of(
           ChainedRelMetadataProvider.of(
               ImmutableList.of(
+                  SOURCE_ROW_COUNT,
+                  new HiveMaterializationRelMdCost().getMetadataProvider(),
                   SOURCE_NODE_TYPES,
                   SOURCE_EXPRESSION_LINEAGE,
                   SOURCE_ALL_PREDICATES,
@@ -115,6 +130,75 @@ public class HiveMaterializationRelMetadataProvider {
         }
       }
       return mq.getTableReferences(Util.first(rel.getBest(), rel.getOriginal()));
+    }
+  }
+
+  private static class HiveRelMdRowCount extends RelMdRowCount {
+    private static final Logger LOG  = LoggerFactory.getLogger(HiveRelMdRowCount.class);
+
+    @Override
+    public Double getRowCount(TableScan rel, RelMetadataQuery mq) {
+      if ("mv_filter".equals(rel.getTable().getQualifiedName().get(1))) {
+        LOG.info("Returning null for mv_filter row count ");
+        return Double.POSITIVE_INFINITY;
+      }
+      try {
+        LOG.info("Estimate row count of {}", rel.getTable().getQualifiedName().get(1));
+        return rel.estimateRowCount(mq);
+      }
+      catch (Exception ex) {
+        LOG.warn("Error while getting row count from rel " + rel, ex);
+        return Double.POSITIVE_INFINITY;
+      }
+    }
+  }
+
+  private static class HiveMaterializationRelMdCost extends HiveRelMdCost {
+
+    public HiveMaterializationRelMdCost() {
+      super(HiveDefaultCostModel.getCostModel());
+    }
+
+    @Override
+    public RelMetadataProvider getMetadataProvider() {
+      return ChainedRelMetadataProvider.of(
+          ImmutableList.of(
+              ReflectiveRelMetadataProvider.reflectiveSource(this, BuiltInMethod.NON_CUMULATIVE_COST.method),
+              HiveMaterializationRelMdPercentageOriginalRows.SOURCE));
+    }
+  }
+
+  private static class HiveMaterializationRelMdPercentageOriginalRows
+      extends RelMdPercentageOriginalRows {
+    private static final Logger LOG  = LoggerFactory.getLogger(HiveMaterializationRelMdPercentageOriginalRows.class);
+    private static final RelMdPercentageOriginalRows INSTANCE =
+        new HiveMaterializationRelMdPercentageOriginalRows();
+
+    public static final RelMetadataProvider SOURCE =
+        ChainedRelMetadataProvider.of(
+            ImmutableList.of(
+                ReflectiveRelMetadataProvider.reflectiveSource(
+                    BuiltInMethod.PERCENTAGE_ORIGINAL_ROWS.method, INSTANCE),
+
+                ReflectiveRelMetadataProvider.reflectiveSource(
+                    BuiltInMethod.CUMULATIVE_COST.method, INSTANCE),
+
+                ReflectiveRelMetadataProvider.reflectiveSource(
+                    BuiltInMethod.NON_CUMULATIVE_COST.method, INSTANCE)));
+
+    @Override
+    public RelOptCost getNonCumulativeCost(RelNode rel, RelMetadataQuery mq) {
+      if ("mv_filter".equals(rel.getTable().getQualifiedName().get(1))) {
+        LOG.info("Returning null for mv_filter row count ");
+        return HiveCost.INFINITY;
+      }
+      try {
+        return super.getNonCumulativeCost(rel, mq);
+      }
+      catch (Exception ex) {
+        LOG.warn("Error while calculating non cumulative cost of " + rel, ex);
+        return HiveCost.INFINITY;
+      }
     }
   }
 }
