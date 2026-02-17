597aa37f45b598c163f3fdfd73b21b05a7893c34
kasakrisz <kasakrisz2@gmail.com>
2026-02-17 15:21:32 +0100

    HIVE-29447: Incorrect nullability inference when transforming SEARCH expression back to disjunctions

--- ql/src/java/org/apache/hadoop/hive/ql/optimizer/calcite/rules/HiveRexShuttleTransformRule.java
+++ ql/src/java/org/apache/hadoop/hive/ql/optimizer/calcite/rules/HiveRexShuttleTransformRule.java
@@ -17,12 +17,22 @@
  */
 package org.apache.hadoop.hive.ql.optimizer.calcite.rules;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 import org.apache.calcite.plan.RelOptRuleCall;
 import org.apache.calcite.plan.RelRule;
 import org.apache.calcite.rel.RelNode;
+import org.apache.calcite.rel.type.RelDataTypeField;
 import org.apache.calcite.rex.RexBuilder;
+import org.apache.calcite.rex.RexInputRef;
+import org.apache.calcite.rex.RexNode;
 import org.apache.calcite.rex.RexShuttle;
+import org.apache.hadoop.hive.ql.optimizer.calcite.HiveRelFactories;
+import org.apache.hadoop.hive.ql.optimizer.calcite.reloperators.HiveProject;
+import org.apache.ivy.core.deliver.DeliverEngineSettings;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 import java.util.function.Function;
 
@@ -35,9 +45,38 @@ public class HiveRexShuttleTransformRule extends RelRule<HiveRexShuttleTransform
   public void onMatch(final RelOptRuleCall call) {
     RelNode startNode = call.rel(0);
     RelNode rewriteNode = startNode.accept(config.shuttleFactory.apply(startNode.getCluster().getRexBuilder()));
-    if (startNode != rewriteNode) {
+
+    if (startNode == rewriteNode) {
+      return;
+    }
+
+    RexBuilder rexBuilder = call.builder().getRexBuilder();
+    List<RexNode> rootFieldList = new ArrayList<>(startNode.getRowType().getFieldCount());
+    List<String> newColumnNames = new ArrayList<>(startNode.getRowType().getFieldCount());
+    boolean nullabilityCastNeeded = false;
+    for (int i = 0; i < startNode.getRowType().getFieldCount(); ++i) {
+      RelDataTypeField startColType = startNode.getRowType().getFieldList().get(i);
+      RelDataTypeField rewriteColType = rewriteNode.getRowType().getFieldList().get(i);
+      RexNode rewriteInputRef = rexBuilder.makeInputRef(rewriteColType.getType(), i);
+
+      if (startColType.getType().equals(rewriteColType.getType())) {
+        rootFieldList.add(rewriteInputRef);
+      } else {
+        rootFieldList.add(rexBuilder.makeCast(startColType.getType(), rewriteInputRef));
+        nullabilityCastNeeded = true;
+      }
+
+      newColumnNames.add(rewriteColType.getName());
+    }
+
+    if (!nullabilityCastNeeded) {
       call.transformTo(rewriteNode);
     }
+
+    RelNode rewriteNodeWithCast = HiveRelFactories.HIVE_PROJECT_FACTORY.
+            createProject(rewriteNode, ImmutableList.of(), rootFieldList, newColumnNames, ImmutableSet.of());
+
+    call.transformTo(rewriteNodeWithCast);
   }
 
   public static class Config extends HiveRuleConfig {
